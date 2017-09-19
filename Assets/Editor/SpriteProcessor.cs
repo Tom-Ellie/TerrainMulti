aea8f63d08923b9dcdb466587303b6f1a82ddba7
Tom Camp <tomcamp333@gmail.com>
2017-09-19 19:58:11 +0100

    Added basic perlin noise

---
+++ Assets/Editor/SpriteProcessor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SpriteProcessor : AssetPostprocessor {
+
+    //Any time a texture is imported into project, this is called.
+    //Store asset path in string, check if it contains "sprites", get ref to textureimporter, then set textureimporter to sprite
+    void OnPostprocessTexture(Texture2D texture) {
+        string lowerCaseAssetPath = assetPath.ToLower(); //assetPath is from AssetPostprocessor
+        bool isInSpritesDirectory = (lowerCaseAssetPath.IndexOf("/sprites/") != -1); 
+
+        if (isInSpritesDirectory) {
+            TextureImporter textureImporter = (TextureImporter)assetImporter; //assetImporter is part of AssetPostprocessor
+            textureImporter.textureType = TextureImporterType.Sprite;
+        }
+    }
+}
