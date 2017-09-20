3867ab606030065cd5918265eab63ca83083d7ad
Tom Camp <tomcamp333@gmail.com>
2017-09-20 09:50:48 +0100

    Added ability to save and load from a binary file in GameControl

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,12 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameControl : MonoBehaviour {
 
     public static GameControl control; //Ensures this is single instance
                                        // Use this for initialization
 
-    public int valueStored;
+    public float valueStored;
 
 	void Awake () {
         if (control == null) { //If no control exists...
@@ -21,4 +24,32 @@ public class GameControl : MonoBehaviour {
 	void OnGUI () {
         GUI.Label(new Rect(10,10,100,30), "Val stored: " + valueStored);
 	}
+
+    //Not super secure if serialized. Should encrypt etc.
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Create);
+
+        PlayerData data = new PlayerData();
+        data.valueStored = valueStored;
+        Debug.Log(Application.persistentDataPath);
+        bf.Serialize(file, data); //Sends data as binary to file
+        file.Close();
+    }
+
+    public void Load() {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+            valueStored = data.valueStored;
+        }
+    }
+}
+
+[Serializable] //Tells unity this class can be written to a file
+class PlayerData {
+    public float valueStored;
 }
---
+++ Assets/Scripts/TestButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestButton : MonoBehaviour {
+
+    void OnGUI() {
+        if (GUI.Button(new Rect(10, 100, 100, 30), "Save")) {
+            GameControl.control.Save();
+        }
+        if (GUI.Button(new Rect(10, 140, 100, 30), "Load")) {
+            GameControl.control.Load();
+        }
+    }
+}
