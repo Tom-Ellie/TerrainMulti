17798755ad057499a3bd17d4c7529eb03f416920
Tom Camp <tomcamp333@gmail.com>
2017-09-20 08:43:46 +0100

    Added game controller, which can store game state through scenes

---
+++ Assets/Editor/SceneCreationSetup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class SceneCreationSetup : MonoBehaviour {
+
+    [PostProcessSceneAttribute(2)]
+    public static void OnPostprocessScene(){
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0.0f, 0.5f, 0.0f);
+    }
+}
---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control; //Ensures this is single instance
+                                       // Use this for initialization
+
+    public int valueStored;
+
+	void Awake () {
+        if (control == null) { //If no control exists...
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        } else if (control != this) { //If this isn't the control...
+            Destroy(gameObject);
+        }
+	}
+	
+	// Testing using gui
+	void OnGUI () {
+        GUI.Label(new Rect(10,10,100,30), "Val stored: " + valueStored);
+	}
+}
