cd968bc52a360a36a51bbd5db5d32e3cea1f8b87
tomcamp333 <32105679+tomcamp333@users.noreply.github.com>
2017-09-21 17:54:25 +0100

    Constant generation and MORE
    Endless terrain, Falloff, normal fixing with borders.

--- Assets/Scripts/ProcGen/TextureGenerator.cs
+++ Assets/Scripts/ProcGen/TextureGenerator.cs
@@ -17,7 +17,7 @@ public static class TextureGenerator {
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
 
-        Texture2D texture = new Texture2D(width, height);
+ //       Texture2D texture = new Texture2D(width, height);
 
         Color[] colourMap = new Color[width * height];
         for (int y = 0; y < height; y++) {
