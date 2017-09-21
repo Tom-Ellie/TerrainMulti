f74564bcbd5ab4e2ffd6e322e8752483e4ee198f
tomcamp333 <32105679+tomcamp333@users.noreply.github.com>
2017-09-21 14:49:32 +0100

    Basic implemeneted
    Basic colouring for heights, uses Perlin noise. Has scales, seed usage, mesh height curve, etc. Also has level of detail added.

---
+++ Assets/Scripts/ProcGen/MapGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public enum DrawMode { NoiseMap, ColourMap, Mesh};
+    public DrawMode drawMode;
+
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail; //Used for scaling
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve; //Height curve used to decide influence of scale at height
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++) {
+            for (int x = 0; x < mapChunkSize; x++) {
+                float currentHeight = noiseMap[x, y];
+                for(int i = 0; i < regions.Length; i++) {
+                    if(currentHeight <= regions[i].height) {
+                        colourMap[y * mapChunkSize + x] = regions[i].colour;
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        } else if (drawMode == DrawMode.ColourMap) {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        } else if (drawMode == DrawMode.Mesh) {
+            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        }
+    }
+    
+    //Called when value changed in interface
+    private void OnValidate() {
+        if(lacunarity<1) { lacunarity = 1; }
+        if(octaves < 0) { octaves = 0; }
+    }
+
+    [System.Serializable] //Ensures it shows up in inspector
+    public struct TerrainType {
+        public string name;
+        public float height;
+        public Color colour;
+    }
+}
